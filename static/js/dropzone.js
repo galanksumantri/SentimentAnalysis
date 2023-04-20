50d18d21214e052b8dfafcbc3fa54c3faef8f571
galanksumantri <galanksumantri@gmail.com>
2023-04-20 11:04:21 +0700

    styling

--- static/js/dropzone.js
+++ static/js/dropzone.js
@@ -1,4 +1,7 @@
 const dropzone = document.querySelector('.dropzone');
+const filename = document.querySelector('.filename');
+const dropzoneHint = document.querySelector('.dropzone-hint');
+const droparea = document.querySelector('.droparea');
 
 dropzone.addEventListener('dragover', e => {
     e.preventDefault();
@@ -15,6 +18,23 @@ dropzone.addEventListener('drop', e => {
     dropzone.classList.remove('dragover');
     const fileInput = document.querySelector('input[type="file"]');
     fileInput.files = e.dataTransfer.files;
-    const submitForm = document.querySelector('form');
-    submitForm.submit();
+    const formData = new FormData();
+    formData.append('file', fileInput.files[0]);
+    fetch('/upload', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('File uploaded successfully');
+        } else {
+            console.error('Error uploading file');
+        }
+    })
+    .catch(error => console.error(error));
+    filename.textContent = fileInput.files[0].name;
+    dropzoneHint.classList.add('hidden');
+    droparea.classList.remove('bg-white');
+    droparea.classList.add('greenarea');
+    submitBtn.disabled = false;
 });
